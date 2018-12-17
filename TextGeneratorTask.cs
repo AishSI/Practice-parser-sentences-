72435b188ecd5a6b96dc99304052ae7b78e3e63f
AishSI <aishsi@gmail.com>
2018-12-17 21:49:21 +0700

    Почти сделан текст-генератор. Все работает, но нужен обработчик начальной фразы: требуется разбивать ее на слова.

--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -2,14 +2,32 @@
 
 namespace TextAnalysis
 {
-    static class TextGeneratorTask
-    {
-        public static string ContinuePhrase(
-            Dictionary<string, string> nextWords,
-            string phraseBeginning,
-            int wordsCount)
-        {
-            return phraseBeginning;
-        }
-    }
+	static class TextGeneratorTask
+	{
+		public static string ContinuePhrase(Dictionary<string, string> nextWords, string phraseBeginning, int wordsCount)
+		{
+			LinkedList<string> intermediatePhrase = new LinkedList<string>();
+			intermediatePhrase.AddLast(phraseBeginning);
+
+			for (var i = 0; i < wordsCount; i++)
+			{
+				if (intermediatePhrase.Count > 1)
+					{
+					if (nextWords.ContainsKey(intermediatePhrase.Last.Previous.Value + " " + intermediatePhrase.Last.Value))
+						intermediatePhrase.AddLast(nextWords[intermediatePhrase.Last.Previous.Value + " " + intermediatePhrase.Last.Value]);
+					else if (nextWords.ContainsKey(intermediatePhrase.Last.Value))
+						intermediatePhrase.AddLast(nextWords[intermediatePhrase.Last.Value]);
+					else return phraseBeginning;
+					phraseBeginning += " " + intermediatePhrase.Last.Value.ToString();
+					}
+				if (intermediatePhrase.Count == 1)
+					if (nextWords.ContainsKey(intermediatePhrase.Last.Value))
+				{
+					intermediatePhrase.AddLast(nextWords[intermediatePhrase.Last.Value]);
+					phraseBeginning += " " + intermediatePhrase.Last.Value.ToString();
+				}
+			}
+			return phraseBeginning;
+		}
+	}
 }
