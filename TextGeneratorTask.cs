5c5b6cd5b2a4266b0387220b83ee26004619ce82
AishSI <aishsi@gmail.com>
2018-12-17 22:27:07 +0700

    Доделал практику "продолжение текста"

--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -6,26 +6,22 @@ namespace TextAnalysis
 	{
 		public static string ContinuePhrase(Dictionary<string, string> nextWords, string phraseBeginning, int wordsCount)
 		{
+			var listWords = phraseBeginning.ToLower().Split(new char[] { ' ' });
 			LinkedList<string> intermediatePhrase = new LinkedList<string>();
-			intermediatePhrase.AddLast(phraseBeginning);
-
+			foreach (var w in listWords)
+				intermediatePhrase.AddLast(w);
 			for (var i = 0; i < wordsCount; i++)
 			{
-				if (intermediatePhrase.Count > 1)
-					{
-					if (nextWords.ContainsKey(intermediatePhrase.Last.Previous.Value + " " + intermediatePhrase.Last.Value))
-						intermediatePhrase.AddLast(nextWords[intermediatePhrase.Last.Previous.Value + " " + intermediatePhrase.Last.Value]);
-					else if (nextWords.ContainsKey(intermediatePhrase.Last.Value))
-						intermediatePhrase.AddLast(nextWords[intermediatePhrase.Last.Value]);
-					else return phraseBeginning;
-					phraseBeginning += " " + intermediatePhrase.Last.Value.ToString();
-					}
-				if (intermediatePhrase.Count == 1)
-					if (nextWords.ContainsKey(intermediatePhrase.Last.Value))
-				{
+				if (nextWords.ContainsKey((intermediatePhrase.Count > 1
+					? intermediatePhrase.Last.Previous.Value
+					: "") + " " + intermediatePhrase.Last.Value))
+					intermediatePhrase.AddLast(
+						nextWords[intermediatePhrase.Last.Previous.Value 
+						+ " " + intermediatePhrase.Last.Value]);
+				else if (nextWords.ContainsKey(intermediatePhrase.Last.Value))
 					intermediatePhrase.AddLast(nextWords[intermediatePhrase.Last.Value]);
-					phraseBeginning += " " + intermediatePhrase.Last.Value.ToString();
-				}
+				else return phraseBeginning;
+				phraseBeginning += " " + intermediatePhrase.Last.Value.ToString();
 			}
 			return phraseBeginning;
 		}
